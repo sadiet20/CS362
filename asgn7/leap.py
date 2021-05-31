8f95b3fd1528445911522ddc63c4fde3e7225122
sadiet20 <sadierosethomas@gmail.com>
2021-05-30 19:59:33 -0700

    passed third test: return false if divisible by 100

--- asgn7/leap.py
+++ asgn7/leap.py
@@ -7,6 +7,8 @@ Description: demonstrates test-first development with leap year program
 
 
 def is_leap(year):
+	if(year % 100 == 0):
+		return False
 	if(year % 4 == 0):
 		return True
 	return False
