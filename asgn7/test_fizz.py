884033650c631a53979dd335d6b7abb9d71af779
sadiet20 <sadierosethomas@gmail.com>
2021-05-30 19:36:20 -0700

    added second test: does 17 return 'seventeen'

--- asgn7/test_fizz.py
+++ asgn7/test_fizz.py
@@ -6,13 +6,14 @@ Description: demonstrate test-first development with FizzBuzz program
 '''
 
 import unittest
-#fails because fizz is not created yet
 import fizz
 
 
 class testCaseFizz(unittest.TestCase):
 	def test_normal(self):
 		self.assertEqual(fizz.FizzBuzz(2), "two")
+	def test_normal2(self):
+		self.assertEqual(fizz.FizzBuzz(17), "seventeen")
 
 
 if __name__ == '__main__':
