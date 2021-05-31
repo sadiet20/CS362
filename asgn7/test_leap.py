90174005f1a298e581f096592642b0df4972dc64
sadiet20 <sadierosethomas@gmail.com>
2021-05-30 19:56:33 -0700

    wrote second test: divisible by four

--- asgn7/test_leap.py
+++ asgn7/test_leap.py
@@ -6,14 +6,14 @@ Description: demonstrates test-first development with leap year program
 '''
 
 import unittest
-#fails because leap does not exist
 import leap
 
 
 class testCaseLeap(unittest.TestCase):
 	def test_non_leap(self):
 		self.assertFalse(leap.is_leap(5))
-
+	def test_div4(self):
+		self.assertTrue(leap.is_leap(64))
 
 if __name__ == '__main__':
 	unittest.main()
